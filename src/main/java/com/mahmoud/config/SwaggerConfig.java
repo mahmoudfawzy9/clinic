be821b960fda01d679956313d0b06ee0320ccda6
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-03-04 19:29:25 +0200

    organizing

--- src/main/java/com/mahmoud/config/SwaggerConfig.java
+++ src/main/java/com/mahmoud/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.mahmoud.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -9,6 +11,7 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+@Configuration
 public class SwaggerConfig {
     @Bean
     public Docket api() {
