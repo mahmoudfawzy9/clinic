1a034cabd8c24f51b8fd86474e7ae9d3e8083f86
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-03-11 00:20:28 +0200

    Update PatientService.java

--- src/main/java/com/mahmoud/clinic2/service/PatientService.java
+++ src/main/java/com/mahmoud/clinic2/service/PatientService.java
@@ -7,4 +7,7 @@ import org.springframework.http.ResponseEntity;
 public interface PatientService {
 
     ResponseEntity<CreatePatientResponse> insert(CreatePatientProfileRequest createPatientProfileRequest);
+    
+    Patient getPatientById(Long id);
+
 }
