b49dc17ea47ab118a5a0322eb96c198c3442821c
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-03-10 23:20:51 +0200

    organizing

--- src/test/java/com/mahmoud/clinic2/Clinic2ApplicationTests.java
+++ src/test/java/com/mahmoud/clinic2/Clinic2ApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.mahmoud.clinic2;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class Clinic2ApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.mahmoud.clinic2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clinic2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
